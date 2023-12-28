cd5f1de4603683c136f2180d16e823a83aef0f9d
estealvarez <este.alvarez@duocuc.cl>
2023-12-28 19:31:44 -0300

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -55,7 +55,7 @@ get_header();
 		<!--Sección de cuadricula-->
 
 		<!--Ripley puntos-->
-		<section class="ripley-puntos container-fluid my-5 p-5">
+		<section class="ripley-puntos container-fluid my-5 p-3">
 			<div class="row m-0 p-0">
 				<div class="col-12 col-md-4 d-flex justify-content-center align-items-center">
 					<a href="<?php the_field('link_ripley');?>">
