443d6355d52bc701cee7f611c3971c3010d6d4e3
estealvarez <este.alvarez@duocuc.cl>
2024-01-02 18:28:52 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -21,7 +21,7 @@
 				<a href="https://www.instagram.com/fundacioncbh/" target="_blank"><i class="bi bi-instagram fs-2 px-5"></i></a>
 				<a href="https://www.tiktok.com/@fundacioncbh" target="_blank"><i class="bi bi-tiktok fs-2"></i></a>
 			</div>
-			<div class="col-12 my-3 correo">
+			<div class="col-12 mt-3 correo">
 				<p><i class="fa-regular fa-envelope"></i>contacto@fundacioncbh.cl</p>
 				<a class="fw-semibold" href="http://fundacioncbh.cl/admin/login.php" target="_blank">Registro</a>
 				<p class="pt-3">Obtén información sobre la Ley de Tenencia Responsable de Mascotas y Animales de Compañía haciendo clic <a class="aqui" href="https://www.chileatiende.gob.cl/fichas/51436-ley-de-tenencia-responsable-de-mascotas-y-animales-de-compania-ley-cholito" target="_blank">aquí</a></p>
