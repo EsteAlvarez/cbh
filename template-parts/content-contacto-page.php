d8c11dc47e4a8fe295d24fc852a175d2d55e423e
estealvarez <este.alvarez@duocuc.cl>
2024-01-02 18:41:53 -0300

    Comentarios en página de contacto

--- template-parts/content-contacto-page.php
+++ template-parts/content-contacto-page.php
@@ -12,25 +12,35 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('col-lg-6'); ?>>
 <form class="container" action="post">
     <fieldset class="row">
+        <!--Nombre-->
         <label for="contacto_nombre" class="form-label col-md-6 col-12 mb-3">Nombre: 
             <input class="form-control w-100" id="contacto_nombre" name="contacto_nombre" type="text" placeholder="Ingrese su nombre">
         </label>
+        <!--Nombre-->
 
+        <!--Apellido-->
         <label for="contacto_apellido" class="form-label col-md-6 col-12 mb-3">Apellido: 
             <input class="form-control w-100" id="contacto_apellido" name="contacto_apellido" type="text" placeholder="Ingrese su apellido">
         </label>
+        <!--Apellido-->
 
+        <!--Email-->
         <label for="contacto_email" class="form-label col-md-6 col-12 mb-3">Email: 
             <input class="form-control w-100" id="contacto_email" name="contacto_email" type="email" placeholder="Ingrese su email">
         </label>
+        <!--Email-->
 
+        <!--Teléfono-->
         <label for="contacto_telefono" class="form-label col-md-6 col-12 mb-3">Teléfono: 
             <input class="form-control w-100" id="contacto_telefono" name="contacto_telefono" type="email" placeholder="Ingrese su teléfono">
         </label>
+        <!--Teléfono-->
 
+        <!--Mensaje-->
         <label for="contacto_mensaje">Mensaje: 
             <textarea class="form-control" name="contacto_mensaje" id="contacto_mensaje" cols="30" rows="10" placeholder="Ingrese su mensaje"></textarea>
         </label>
+        <!--Mensaje-->
     </fieldset>
 </form>
 </article><!-- #post-<?php the_ID(); ?> -->
